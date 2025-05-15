863dc0e6f021d1c789795800f5ead6f98ee5d697
astrit-os <astrit_osmani@hotmail.com>
2025-05-15 23:01:16 +0200

    test4

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hallo, dies ist ein Test -ASO   --test3');
+  res.send('Hallo, dies ist ein Test -ASO   --test4');
 });
 
 app.listen(PORT, HOST);
