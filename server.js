2aa7476eabf622140649845215ac0a529d429446
astrit-os <astrit_osmani@hotmail.com>
2025-05-16 18:12:09 +0200

    änderung für azure web app

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hallo, dies ist ein Test -ASO   W13 Cloud Deployment with GitHub Actions');
+  res.send('Hallo, dies ist ein Test -ASO   W13 Cloud Deployment with GitHub Actions -v2 !!');
 });
 
 app.listen(PORT, HOST);
